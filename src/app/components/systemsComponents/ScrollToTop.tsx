0f15309e3aaa1454191a3b4bcff7cd61d37a0729
Cristhian S. Schröer <81269993+Cristhian-Shr@users.noreply.github.com>
2025-03-12 11:54:56 -0300

    novo componente scrollToTop

---
+++ src/app/components/systemsComponents/ScrollToTop.tsx
@@ -0,0 +1,36 @@
+"use client";
+
+import { useEffect, useState } from "react";
+import { FaArrowUp } from "react-icons/fa";
+
+export default function ScrollToTop() {
+  const [isVisible, setIsVisible] = useState(false);
+
+  useEffect(() => {
+    const toggleVisibility = () => {
+      if (window.scrollY > 300) {
+        setIsVisible(true);
+      } else {
+        setIsVisible(false);
+      }
+    };
+
+    window.addEventListener("scroll", toggleVisibility);
+    return () => window.removeEventListener("scroll", toggleVisibility);
+  }, []);
+
+  const scrollToTop = () => {
+    window.scrollTo({ top: 0, behavior: "smooth" });
+  };
+
+  return (
+    <button
+      onClick={scrollToTop}
+      className={`fixed bottom-32 right-20 bg-brownBgNav text-white p-4 rounded-full shadow-lg transition-opacity duration-300 ${
+        isVisible ? "opacity-100" : "opacity-0 pointer-events-none"
+      }`}
+    >
+      <FaArrowUp className="text-xl" />
+    </button>
+  );
+}
