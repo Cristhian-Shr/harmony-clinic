371197e9901cc3b1db561ae7ac04ba4ffb3fb556
Cristhian S. Schröer <81269993+Cristhian-Shr@users.noreply.github.com>
2025-03-12 11:42:13 -0300

    ajuste em rotas mobile

--- src/app/components/headerComponents/dropdownMenuCel.tsx
+++ src/app/components/headerComponents/dropdownMenuCel.tsx
@@ -5,61 +5,60 @@ import {
   DropdownMenuItem,
   DropdownMenuLabel,
   DropdownMenuSeparator,
-  DropdownMenuShortcut,
   DropdownMenuTrigger,
-} from "@/components/ui/dropdown-menu"
-import { HiOutlineBuildingOffice2 } from "react-icons/hi2"
-import { IoMenu } from "react-icons/io5"
-import { MdOutlinePhoneAndroid } from "react-icons/md"
-import { SiGithubsponsors } from "react-icons/si"
+} from "@/components/ui/dropdown-menu";
+import Link from "next/link";
+import { HiOutlineBuildingOffice2 } from "react-icons/hi2";
+import { IoMenu } from "react-icons/io5";
+import { MdOutlinePhoneAndroid } from "react-icons/md";
+import { SiGithubsponsors } from "react-icons/si";
 
 export function DropdownMenuCel() {
   return (
     <DropdownMenu>
       <DropdownMenuTrigger asChild>
-        <button><IoMenu className="text-5xl"/></button>
+        <button>
+          <IoMenu className="text-5xl" />
+        </button>
       </DropdownMenuTrigger>
       <DropdownMenuContent className="w-80 bg-brownBgNav text-writingOne mr-20 mt-5">
-        <DropdownMenuLabel className="text-center">Harmony Clinic Menu</DropdownMenuLabel>
+        <DropdownMenuLabel className="text-center">
+          Harmony Clinic Menu
+        </DropdownMenuLabel>
         <DropdownMenuSeparator />
         <DropdownMenuGroup>
-          <DropdownMenuItem className="hover:text-brownBgNav hover:bg-writingOne duration-500 font-semibold">
-            Sobre
-            <DropdownMenuShortcut><HiOutlineBuildingOffice2 /></DropdownMenuShortcut>
+          <DropdownMenuItem asChild>
+            <Link href="#about" className="hover:text-brownBgNav hover:bg-writingOne duration-500 font-semibold flex mx-4">
+              Sobre
+              <HiOutlineBuildingOffice2 className="ml-auto" />
+            </Link>
           </DropdownMenuItem>
-          <DropdownMenuItem className="hover:text-brownBgNav hover:bg-writingOne duration-500 font-semibold">
-          Depoimentos
-          <DropdownMenuShortcut><SiGithubsponsors /></DropdownMenuShortcut>
+          <DropdownMenuItem asChild>
+            <Link href="#depoiments" className="hover:text-brownBgNav hover:bg-writingOne duration-500 font-semibold flex mx-4">
+              Depoimentos
+              <SiGithubsponsors className="ml-auto" />
+            </Link>
           </DropdownMenuItem>
-          <DropdownMenuItem className="hover:text-brownBgNav hover:bg-writingOne duration-500 font-semibold">
-          Resultados
-            <DropdownMenuShortcut><MdOutlinePhoneAndroid /></DropdownMenuShortcut>
+          <DropdownMenuItem asChild>
+            <Link href="#resultosCorporais" className="hover:text-brownBgNav hover:bg-writingOne duration-500 font-semibold flex mx-4">
+              Resultados Corporais
+              <MdOutlinePhoneAndroid className="ml-auto" />
+            </Link>
           </DropdownMenuItem>
-          <DropdownMenuItem className="hover:text-brownBgNav hover:bg-writingOne duration-500 font-semibold">
-          Contato
-            <DropdownMenuShortcut><MdOutlinePhoneAndroid /></DropdownMenuShortcut>
+          <DropdownMenuItem asChild>
+            <Link href="#resultosFaciais" className="hover:text-brownBgNav hover:bg-writingOne duration-500 font-semibold flex mx-4">
+              Resultados Faciais
+              <MdOutlinePhoneAndroid className="ml-auto" />
+            </Link>
+          </DropdownMenuItem>
+          <DropdownMenuItem asChild>
+            <Link href="#contact" className="hover:text-brownBgNav hover:bg-writingOne duration-500 font-semibold flex mx-4">
+              Contato
+              <MdOutlinePhoneAndroid className="ml-auto" />
+            </Link>
           </DropdownMenuItem>
         </DropdownMenuGroup>
-        <DropdownMenuSeparator />
-        {/* <DropdownMenuGroup>
-          <DropdownMenuItem className="hover:text-brownBgNav hover:bg-writingOne duration-500 font-semibold">Team</DropdownMenuItem>
-          <DropdownMenuSub>
-            <DropdownMenuSubTrigger className="hover:text-brownBgNav hover:bg-writingOne duration-500 font-semibold">Invite users</DropdownMenuSubTrigger>
-            <DropdownMenuPortal>
-              <DropdownMenuSubContent>
-                <DropdownMenuItem className="hover:text-brownBgNav hover:bg-writingOne duration-500 font-semibold">Email</DropdownMenuItem>
-                <DropdownMenuItem className="hover:text-brownBgNav hover:bg-writingOne duration-500 font-semibold">Message</DropdownMenuItem>
-                <DropdownMenuSeparator className="text-white border-b" />
-                <DropdownMenuItem className="hover:text-brownBgNav hover:bg-writingOne duration-500 font-semibold">More...</DropdownMenuItem>
-              </DropdownMenuSubContent>
-            </DropdownMenuPortal>
-          </DropdownMenuSub>
-          <DropdownMenuItem>
-            New Team
-            <DropdownMenuShortcut>⌘+T</DropdownMenuShortcut>
-          </DropdownMenuItem>
-        </DropdownMenuGroup> */}
       </DropdownMenuContent>
     </DropdownMenu>
-  )
+  );
 }
